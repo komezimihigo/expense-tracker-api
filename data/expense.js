51fe31fdba96c935d01c623bf48f29f6fd5765c1
nhanakhay <theology75@gmail.com>
2026-07-21 16:40:27 +0000

    added expense.js to show sample data of the content of the objects in the array

---
+++ data/expense.js
@@ -0,0 +1,13 @@
+let expenseData = [
+    {
+
+        //sample data so team knows which data to place in array
+        id:0,
+        title:"Khebab",
+        amount:100,
+        category: "Food",
+        date:"2026-07-20"
+        
+    }
+]
+
